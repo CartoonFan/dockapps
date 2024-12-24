f6bc2293c1dbe79853dc950a57e2554c6bfb77e9
Jeremy Sowden <jeremy@azazel.net>
2024-12-24 15:55:45 +0000

    wmmoonclock: fix type of arguments to `gmtime(3)` and `localtime(3)`
    With recent versions of gcc, on architectures where `long` and `time_t` are not
    the same size, passing pointers to `long` to `gmtime(3)` and `localtime(3)`,
    leads to "incompatible pointer type" errors.  Use `time_t` instead.
    
    Link: https://bugs.debian.org/1091282
    Signed-off-by: Jeremy Sowden <jeremy@azazel.net>

--- wmmoonclock/src/wmMoonClock.c
+++ wmmoonclock/src/wmMoonClock.c
@@ -157,7 +157,8 @@ int main(int argc, char *argv[]) {
     struct tm		*GMTTime, *LocalTime;
     XEvent		event;
     int			i, n, j, ImageNumber, Year, Month, DayOfMonth, digit;
-    long		CurrentLocalTime, CurrentGMTTime, date;
+    time_t		CurrentLocalTime, CurrentGMTTime;
+    long		date;
     double		UT, val, RA, DEC, UTRise, UTSet, LocalHour, hour24();
     int			D, H, M, S, sgn, A, B, q;
     CTrans           	c;
